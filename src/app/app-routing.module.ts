84a7baa9d1ce965557923dedde72b7b6d3182684
amarsankars <sankar.samyappan@gmail.com>
2018-03-16 08:37:54 -0400

    Route to assessment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { QuestviewComponent } from "./questview/questview.component";
 import { ResultviewComponent } from "./resultview/resultview.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/assessement', pathMatch: 'full' },
   { path: 'assessement', component: QuestviewComponent },
   { path: 'results', component: ResultviewComponent }
 ];
